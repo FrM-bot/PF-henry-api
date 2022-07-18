42ae235dcfbfe9b31f0e7ca6fdadb0821518851f
francomei <97065933+francomei@users.noreply.github.com>
2022-07-18 17:09:26 -0300

    Revert "Rutas"

--- src/index.js
+++ src/index.js
@@ -33,8 +33,6 @@ app.get('/b', async (req, res) => {
   res.json(data)
 })
 
-//francomei
-
 app.post('/addBank', async (req, res) => {
   const bank = req.body
   const data = await prisma.bank.create({
