9794338622e0c00350451365975b8a0ccc41bcb6
Agustin Vazquez <agustinvsexto196@gmail.com>
2022-07-19 19:21:15 -0300

    feat():add_post_route_user

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,28 +1,60 @@
-import { Router} from 'express';
+import { Router } from 'express'
 import { PrismaClient } from '@prisma/client'
+import bcrypt from 'bcrypt'
 const prisma = new PrismaClient()
 
+const router = Router()
 
-const router = Router();
+router.post('/signin', async (req, res) => {
+  const { email, password, name, lastname, username } = req.body
+  if (!email || !password || !name || !lastname || !username) {
+    res.status(404).json({ msg: 'Required info is never sent' })
+  }
+  const hashedPass = await bcrypt.hash(password, 10)
+  try {
+    const newUser = await prisma.user.create({
+      data: {
+        email,
+        name,
+        username,
+        lastname,
+        password: hashedPass,
+        profilepic: 'www.sdhgfdoug.com/odshnfgd',
+        accounts: {
+          create: { cvu: '56789312hn21', currenciesIDs: 'we' }
+        }
+      },
+      include: {
+        accounts: true
+      }
+    })
+    if (newUser) {
+      res.status(200).json(newUser)
+    }
+  } catch (error) {
+    console.log(error)
+    res.status(404).json({ msg: 'An error ocurred', error })
+  }
+})
 
 router.get('/', async (req, res) => {
-    const names = ['Franco Maciel']
-    res.status(200).send(names)
+  const names = ['Franco Maciel']
+  res.status(200).send(names)
 })
 
 router.get('/u', async (req, res) => {
-    const data = await prisma.user.findMany({})
-    res.json(data)
+  const data = await prisma.user.findMany({})
+  res.json(data)
 })
 
 router.post('/addUser', async (req, res) => {
-    const user = req.body
-    const data = await prisma.user.create({
+  const user = req.body
+  const data = await prisma.user.create({
     data: {
-    username: user.username
+      username: user.username
     }
-    })
-    res.status(201).json(data)
+  })
+  res.status(201).json(data)
 })
 
-export default router; 
+export default router
