4f365d1e831d2f18a1a3e7029fcca116fc409e10
Matt <maty.britez7@gmail.com>
2022-07-19 17:57:22 -0300

    add crypt route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,13 @@
 import { Router } from 'express'
 import userRoute from './user.js'// esta es la manera de importar rutas de cada archivo accountRoute, typeRoute, etc
 import typesRoute from './type.js'
+import cryptsRoute from './crypts.js'
+
 const router = Router()
 
 router.use('/user', userRoute)// esta es la forma de usar rutas de cada archivo
-router.use('/types', typesRoute)// esta es la forma de usar rutas de cada archivo
+router.use('/types', typesRoute)
+router.use('/crypts', cryptsRoute)// esta es la forma de usar rutas de cada archivo
 
 // OJO: aqui van las otras rutas para cada tabla
 
