859e72372566fb807bd40902afeed2c842cbd004
Agustin Vazquez <agustinvsexto196@gmail.com>
2022-07-20 17:30:15 -0300

    feat(src/routes/movement.js):route-get-movement

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,14 @@ import { Router } from 'express'
 import userRoute from './user.js' // esta es la forma de usar rutas de cada archivo
 import typesRoute from './type.js'
 import cryptsRoute from './crypts.js' // esta es la manera de importar rutas de cada archivo accountRoute, typeRoute, etc
+import movementsRoute from './movement.js'
 
 const router = Router()
 
 router.use('/user', userRoute)// esta es la forma de usar rutas de cada archivo
 router.use('/types', typesRoute)
 router.use('/crypts', cryptsRoute)// esta es la forma de usar rutas de cada archivo
+router.use('/movement', movementsRoute)
 
 // OJO: aqui van las otras rutas para cada tabla
 
