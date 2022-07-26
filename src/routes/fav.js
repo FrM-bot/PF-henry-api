456077b01e16d79d3068edc9da84896da2ac522f
Mateo <mateo.capdevila2801@gmail.com>
2022-07-26 12:30:15 -0300

    routes fav

--- src/routes/fav.js
+++ src/routes/fav.js
@@ -30,6 +30,19 @@ router.get('/:id', async (req, res) => {
 router.post('/createFavourites', async (req, res) => {
   const { id, friendID } = req.body
   try {
+    const existUser = await prisma.user.findUnique({
+      where: {
+        id: friendID
+      }
+    })
+    if (!existUser) return res.status(404).json({ msg: "The user you want to add doesn't exist" })
+    const favAlready = await prisma.fav.findMany({
+      where: {
+        friendID
+      }
+    })
+    console.log(favAlready)
+    if (favAlready.length > 0) return res.status(400).json({ msg: 'Fav already created' })
     const findUser = await prisma.fav.create({
       data: {
         friendID,
@@ -48,12 +61,32 @@ router.post('/createFavourites', async (req, res) => {
 
 router.delete('/delete', async (req, res) => {
   const { id } = req.body
+  console.log('elid', id)
+  const favInfo = await prisma.fav.findMany({
+    where: {
+      friendID: id
+    }
+  })
   const removeFav = await prisma.fav.delete({
     where: {
-      id
+      id: favInfo[0].id
     }
   })
   res.json({ removeFav, msg: 'Favorite deleted' })
+})
+
+router.delete('/xd', async (req, res) => {
+  const { favId } = req.body
+  try {
+    const deleteFav = await prisma.fav.delete({
+      where: {
+        id: favId
+      }
+    })
+    res.json(deleteFav)
+  } catch (error) {
+    console.log(error)
+  }
 })
 
 export default router
