2e10800a2916fe828fa06d63a9feb612e32dc4cd
Franco M <damianmaciel0@gmail.com>
2022-08-07 20:08:59 -0300

    add nodemailer

--- src/routes/user.js
+++ src/routes/user.js
@@ -5,8 +5,7 @@ import jwt from 'jsonwebtoken'
 import userExtractor from '../middlewares/userExtractor.js'
 import { upload, destroy, uploadProfilepic } from '../cloudinaryUpload.js'
 import fs from 'fs/promises'
-import { v2 as cloudinary } from 'cloudinary'
-// import { transporter } from '../config/mailer.js'
+import { transporter } from '../config/mailer.js'
 
 const prisma = new PrismaClient()
 const router = Router()
@@ -518,24 +517,24 @@ router.put('/reset-password', async (req, res) => {
   } catch (err) { console.error(err) }
 })
 
-// router.post('/sendReset', async (req, res) => {
-//   const { email } = req.body
-//   const user = await prisma.user.findUnique({
-//     where: {
-//       email
-//     }
-//   })
-//   if (!user) {
-//     res.status(404).send({ msg: 'not found user' })
-//   } else {
-//     await transporter.sendMail({
-//       from: 'wallet.pfhenry@outlook.com', // sender address
-//       to: `${email}`, // list of receivers
-//       subject: 'Reset password', // Subject line
-//       html: `<h2> Click to the link:  http://localhost:3000/reset/${email} for reset the password.</h2>`
-//     })
-//     res.status(200).send({ msg: 'success' })
-//   }
-// })
+router.post('/sendReset', async (req, res) => {
+  const { email } = req.body
+  const user = await prisma.user.findUnique({
+    where: {
+      email
+    }
+  })
+  if (!user) {
+    res.status(404).send({ msg: 'not found user' })
+  } else {
+    await transporter.sendMail({
+      from: 'wallet.pfhenry@outlook.com', // sender address
+      to: `${email}`, // list of receivers
+      subject: 'Reset password', // Subject line
+      html: `<h2> Click to the link:  http://localhost:3000/reset/${email} for reset the password.</h2>`
+    })
+    res.status(200).send({ msg: 'success' })
+  }
+})
 
 export default router
